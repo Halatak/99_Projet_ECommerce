1f1948394be92de5164331e98ef9fd5c90894a7a
IN-BR-014 <IN-BR-014@IN-BR-014>
2019-03-19 10:18:47 +0100

    Allan: modif DAO produit

--- 99_Projet_ECommerce/src/main/java/fr/adaming/dao/ProduitDaoImpl.java
+++ 99_Projet_ECommerce/src/main/java/fr/adaming/dao/ProduitDaoImpl.java
@@ -2,40 +2,58 @@ package fr.adaming.dao;
 
 import java.util.List;
 
-import javax.persistence.Query;
 
 import org.apache.commons.codec.binary.Base64;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import fr.adaming.model.Categorie;
 import fr.adaming.model.Produit;
 
-
+@Repository
 public class ProduitDaoImpl implements IProduitDao {
 
+	@Autowired
 	private SessionFactory sf;
 	
+	
+	public void setSf(SessionFactory sf) {
+		this.sf = sf;
+	}
+
 	@Override
 	public Produit addProduit(Produit prod) {
-		em.persist(prod);
+		// recuperer le bus(session de hibernate)
+		Session s=sf.getCurrentSession();
+		
+		s.save(prod);
 		return prod;
 	}
 
 	@Override
 	public int deleteProduit(Produit prod) {
+		// recuperer le bus(session de hibernate)
+		Session s=sf.getCurrentSession();
+		
 		String req="DELETE Produit p WHERE p.id=:pId";
 		//recuperer un objet de type query
-		Query queryListe=em.createQuery(req);
+		Query query=s.createQuery(req);
 		
-		queryListe.setParameter("pId", prod.getIdProduit());
-		return queryListe.executeUpdate();
+		query.setParameter("pId", prod.getIdProduit());
+		return query.executeUpdate();
 	}
 
 	@Override
 	public int updateProduit(Produit prod) {
+		// recuperer le bus(session de hibernate)
+		Session s=sf.getCurrentSession();
+		
 		String req="UPDATE Produit p SET p.designation=:pDesignation, p.description=:pDescription, p.prix=:pPrix, p.quantite=:pQuantite, p.selectionne=:pSelectionne, p.photo=:pPhoto WHERE p.id=:pId";
 		//recuperer un objet de type query
-		Query queryListe=em.createQuery(req);
+		Query queryListe=s.createQuery(req);
 		
 		//passage des parametres
 		queryListe.setParameter("pDesignation", prod.getDesignation());
@@ -58,13 +76,16 @@ public class ProduitDaoImpl implements IProduitDao {
 
 	@Override
 	public List<Produit> getAllProduits() {
+		// recuperer le bus(session de hibernate)
+		Session s=sf.getCurrentSession();
+		
 		// construire la requete jsql
 		String req="SELECT prod FROM Produit as prod";
 						
 		//recuperer un objet de type query
-		Query queryListe=em.createQuery(req);
+		Query query=s.createQuery(req);
 		
-		List<Produit> listeProd = queryListe.getResultList();
+		List<Produit> listeProd = query.list();
 		
 		for (Produit p:listeProd){
 			p.setImg("data:image/png;base64,"+Base64.encodeBase64String(p.getPhoto()));
@@ -75,13 +96,15 @@ public class ProduitDaoImpl implements IProduitDao {
 
 	@Override
 	public List<Produit> getProdByCat(Categorie cat) {
+		// recuperer le bus(session de hibernate)
+		Session s=sf.getCurrentSession();
 		
 		// construire la requete jsql
 		String req="SELECT prod FROM Produit as prod WHERE prod.cat.id=:pIdCat";
 		//recuperer un objet de type query
-		Query queryListe=em.createQuery(req);
+		Query query=s.createQuery(req);
 		
-		List<Produit> listeProdByCat = queryListe.getResultList();
+		List<Produit> listeProdByCat = query.list();
 		
 		for (Produit p:listeProdByCat){
 			p.setImg("data:image/png;base64,"+Base64.encodeBase64String(p.getPhoto()));
@@ -91,15 +114,18 @@ public class ProduitDaoImpl implements IProduitDao {
 
 	@Override
 	public List<Produit> getProdByIdCat(Categorie cat) {
+		// recuperer le bus(session de hibernate)
+		Session s=sf.getCurrentSession();
+				
 		// construire la requete jsql
 		String req="SELECT prod FROM Produit as prod WHERE prod.cat.id=:pIdCat";
 		//recuperer un objet de type query
-		Query queryListe=em.createQuery(req);
+		Query query=s.createQuery(req);
 		
 		//passage des parametres
-		queryListe.setParameter("pIdCat", cat.getIdCategorie());
+		query.setParameter("pIdCat", cat.getIdCategorie());
 				
-		List<Produit> listeProdByCat = queryListe.getResultList();
+		List<Produit> listeProdByCat = query.list();
 				
 		for (Produit p:listeProdByCat){
 					p.setImg("data:image/png;base64,"+Base64.encodeBase64String(p.getPhoto()));
